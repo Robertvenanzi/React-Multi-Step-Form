c3a701b10a70b13076c17e708798ba70c4cea828
robertvenanzi <robertgvenanzi@gmail.com>
2018-11-07 20:58:11 -0800

    created nextStep, prevStep, hanndleChange

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -7,9 +7,30 @@ export class UserForm extends Component {
         email:  "",
         occupation:  "",
         city:  "",
-        bio:  ""
-        
+        bio:  ""   
     }
+
+    // Proceed to the next step
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 1
+        });
+    }
+
+    // Go back to prev step 
+      prevStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step - 1
+        });
+    }
+
+    //Handle fields change
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value});
+    }
+
   render() {
     return (
       <div>
